ebe37d983089dc22caa1f37cb6e56026e2f9427e
Peaksol <1244050218@qq.com>
2020-02-28 15:26:19 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # DevicePrefix
-A PocketMine-MP plugin that displays the player's device name before his username. (Eg. [Win10]Steve OR [Android]Alex)
+DevicePrefix is a plugin for PocketMine-MP that displays the player's device name before the username.
+
+Examples: `[Win10]Steve`, `[Android]Alex`, `[iOS]Notch`
+
+Also, everyone will see a join message like `Steve joined the server with a Win10 device.`
+
+# How to use
+1. Download the latest release [here](https://github.com/TravinDreek/DevicePrefix/releases)
+2. Put the .phar file to your `plugins` folder
+3. Restart the server
+
+# Known devices
+The following devices can be identified:
+- Android
+- iOS
+- Mac
+- FireOS
+- GearVR
+- HoloLens
+- Win10
+- Windows(Win32)
+- Dedicated
+- tvOS
+- PS4
+- Switch
+- Xbox
---
+++ src/deviceprefix/DevicePrefix.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DevicePrefix;
+
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\LoginPacket;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+
+class DevicePrefix extends PluginBase implements Listener {
+
+	private $playerDevices; // Usage: $playerDevices['Steve']['DeviceNumber'] OR $playerDevices['Alex']['DeviceName']
+
+	private function getDeviceNameByNumber(int $id) {
+		static $deviceNames = array(1 => 'Android', 'iOS', 'Mac', 'FireOS', 'GearVR', 'HoloLens', 'Win10', 'Windows', 'Dedicated', 'tvOS', 'PS4', 'Switch', 'Xbox'); // Hope the list is all right
+		return $deviceNames[$id] ?? 'unknown';
+	}
+
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onDisable() {
+		unset($this->playerDevices);
+	}
+
+	public function DataPacketReceive(DataPacketReceiveEvent $event) {
+		$packet = $event->getPacket();
+		if($packet instanceof LoginPacket) {
+			$this->playerDevices[$packet->username]['DeviceNumber'] = $packet->clientData['DeviceOS'];
+			$this->playerDevices[$packet->username]['DeviceName'] = $this->getDeviceNameByNumber($this->playerDevices[$packet->username]['DeviceNumber']);
+		}
+	}
+
+	public function PlayerJoin(PlayerJoinEvent $event) {
+		$player = $event->getPlayer();
+		$playerName = $player->getName();
+		$this->getServer()->broadcastMessage(sprintf(
+			'§a%s joined the server with a §e%s§a device.', // Join message format
+			$playerName, $this->playerDevices[$playerName]['DeviceName']
+		));
+		$player->setDisplayName(sprintf(
+			'§a[%s§a] §r%s', // Display name format
+			$this->playerDevices[$playerName]['DeviceName'], $playerName
+		));
+	}
+
+}
